13d37c50962e502b4e498f9e542f5547ecb9eea3
skellykiernan <skelly.kiernan@gmail.com>
2017-09-09 17:58:36 +0100

    Initial commit

---
+++ app/src/main/java/com/skeltronics/currencyconverter/MainActivity.java
@@ -0,0 +1,19 @@
+package com.skeltronics.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+        Log.i("Convert", "Start Converting");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
