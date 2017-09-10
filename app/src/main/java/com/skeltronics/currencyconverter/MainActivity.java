700d5ea491b03a93011a499afe51fe8d367368bc
skellykiernan <skelly.kiernan@gmail.com>
2017-09-10 21:04:00 +0100

    Added simple conversion action to app

--- app/src/main/java/com/skeltronics/currencyconverter/MainActivity.java
+++ app/src/main/java/com/skeltronics/currencyconverter/MainActivity.java
@@ -4,11 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    double dollarToEuroRate = 0.91;
+
     public void convertCurrency(View view) {
         Log.i("Convert", "Start Converting");
+        Toast convertedValueToast;
+        EditText enterDollarNumberView = (EditText) findViewById(R.id.enterDollarNumber);
+        String dollarValueString = enterDollarNumberView.getText().toString();
+        double dollarValue = Double.parseDouble(dollarValueString);
+        Log.i("Convert", "Dollar value is " + String.valueOf(dollarValue));
+        double euroValue = dollarValue * dollarToEuroRate;
+        String euroValueString = String.valueOf(euroValue);
+        Log.i("Convert", "Euro value is " + String.valueOf(euroValue));
+        Toast.makeText(MainActivity.this, euroValueString + " Euro", Toast.LENGTH_LONG).show();
+        Log.i("Convert", "Completed Conversion");
+
     }
 
     @Override
